4539a4b88717e072d719e2ea7210bff884c5efe3
Harrison Vieira <hvieira@gruporcp.local>
2020-06-06 19:51:31 -0300

    finalização de intents

--- src/routes/intents.js
+++ src/routes/intents.js
@@ -1,15 +1,17 @@
 const { Router } = require('express')
-const IbmService = new (require('./ibm'))()
+const IbmService = new (require('../services/ibm'))()
 
 class IntentRoute {
   constructor () {
     this.router = Router()
+    this.instatiate()
   }
 
   instatiate () {
     this.router.route('/').get(
       async (req, res, next) => {
-        res.status(200).send(await IbmService.getIntents)
+        const resp = await IbmService.getIntents()
+        res.status(200).send(resp.intents)
       })
   }
 }
